f7b9f223b55a5307c1fd585e0cb8fd5cd6394cfa
diseraczz <pkpeeradol@gmail.com>
2022-04-09 03:09:46 +0700

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "0930911845", "IP");
+$conn = mysqli_connect("localhost", "root", "12345678", "IP");
 if (!$conn) {
   die("Failed to connect to database" . mysqli_error($conn));
 }
